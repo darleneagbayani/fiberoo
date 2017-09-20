8ac1ee6f7ef7eb3b8960ef348a8bf7df7d29c873
darleneagbayani <31275691+darleneagbayani@users.noreply.github.com>
2017-09-19 19:04:42 -1000

    completed

--- fiberoo.js
+++ fiberoo.js
@@ -5,7 +5,7 @@ let second = 1;
 let next = 0;
 
 
-for(i = 0; i < 10; i++) {
+for(i = 0; i < 100; i++) {
   let temp = next;
   next = first + second;
   second = first;
@@ -16,3 +16,16 @@ return list;
 }
 
 console.log(fib());
+
+function numsToStrings(numArr) {
+  return _.map(numArr, function(item) {return `${item}`});
+}
+
+console.log(numsToStrings(fib()));
+
+function numEvenNums(nums) {
+
+  return _.filter(nums, function(num){ return num % 2 == 0; }).length;
+}
+
+console.log(numEvenNums(fib()));
